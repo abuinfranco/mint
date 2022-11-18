7ac6252b31a2dfd1657d049eb93a5b9dbb5f576a
Franco Abuin <49248726+abuinfranco@users.noreply.github.com>
2022-11-17 22:07:46 -0300

    archivo

---
+++ src/test/java/com/grupo5/mint/MintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.mint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
